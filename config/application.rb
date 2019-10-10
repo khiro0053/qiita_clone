d15527920526bbf730ede8a997ca1953103efed5
khiro <khiro0053@gmail.com>
2019-10-10 23:25:29 +0900

    modify application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ module QiitaClone
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.generators do |g|
+      g.template_engine false
+      g.javascripts false
+      g.stylesheets false
+      g.helper true
       g.test_framework :rspec,
                        view_specs: false,
                        routing_specs: false,
@@ -25,3 +29,5 @@ module QiitaClone
     end
   end
 end
+
+config.api_only = true
