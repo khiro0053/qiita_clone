fec433ddffdb0c51b207dda91927f015c10a367d
khiro <khiro0053@gmail.com>
2019-11-13 17:53:05 +0900

    add test & validation to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,8 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { in: 2..50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, format: { with: VALID_EMAIL_REGEX }
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/
+  validates :password, presence: true, length: { in: 8..32 }, format: { with: VALID_PASSWORD_REGEX }
 
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系" do
+    context "名前、メールアドレス、パスワードが入力されている場合" do
+      let(:user) {build(:user)}
+      it "ユーザー登録できる" do
+        expect(user).to be_valid
+      end
+    end
+  end
+
+  describe "名前についてのエラーチェック" do
+    context "名前が未入力の場合" do
+    let(:user) {build(:user, name: nil)}
+      it "ユーザー登録できない" do
+        expect(user).not_to be_valid
+      end
+    end
+    context "名前が1文字の場合" do
+      let(:user) {build(:user, name:"x")}
+      it "ユーザー登録できない" do
+        expect(user).not_to be_valid
+      end
+    end
+    context "名前が51文字以上の場合" do
+      let(:user) {build(:user, name: "x"*51)}
+      it "ユーザー登録できない" do
+        expect(user).not_to be_valid
+      end
+    end
+  end
+
+  describe "メールアドレスのエラーチェック" do
+    context "メールアドレスが入力されていない場合" do
+      let(:user) {build(:user, email: nil)}
+      it "ユーザー登録できない" do
+        expect(user).not_to be_valid
+      end
+    end
+    context "既に同じメールアドレスが登録されている場合" do
+      before do
+        user1 = create(:user, email: "aaa@bbb.ccc")
+      end
+      let(:user2) {build(:user, email: "aaa@bbb.ccc")}
+      it "ユーザー登録できない" do
+        expect(user2).not_to be_valid
+      end
+    end
+    context "メールアドレスのフォーマットが守られていない場合" do
+      let(:user) {build(:user, email: "$#&%@invalid.com")}
+      it "ユーザー登録できない" do
+        expect(user).not_to be_valid
+      end
+    end
+  end
 end
