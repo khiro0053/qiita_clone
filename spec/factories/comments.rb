56eed64c1b3e02771702496ed8a4146afee55cfb
khiro <khiro0053@gmail.com>
2019-11-14 00:06:55 +0900

    add test & validation to comment

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :comment do
-    body { "MyText" }
+    body { Faker::Lorem.paragraph(sentence_count: 10) }
+    user
+    article
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,20 @@
 require "rails_helper"
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "正常系"do
+    context "コメント文がある場合" do
+      let(:comment) {build(:comment)}
+      it "コメント投稿できる" do
+      expect(comment).to be_valid
+      end
+    end
+  end
+  describe "エラーチェック" do
+    context "コメント文がない場合" do
+      let(:comment) {build(:comment, body: nil)}
+      it "コメント投稿できない" do
+        expect(comment).not_to be_valid
+      end
+    end
+  end
 end
