ac26594d61dbe7f944bb30dc2e19c3269845fe00
khiro <khiro0053@gmail.com>
2019-11-19 19:04:48 +0900

    create articles_controller

---
+++ app/controllers/api/v1/articles_controller.rb
@@ -0,0 +1,26 @@
+class Api::V1::ArticlesController < Api::V1::ApiController
+  before_action :set_article, only: [:show, :update, :destroy]
+
+  def index
+    articles = Article.all
+    render json: articles
+  end
+
+  def show
+    render json: @article
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+end
