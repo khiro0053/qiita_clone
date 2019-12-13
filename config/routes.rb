5197ca56de807e70164a0f259a11b2f98e656693
khiro <khiro0053@gmail.com>
2019-12-13 21:02:16 +0900

    Add homes files

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'homes/index'
+  root 'homes#index'
   namespace :api, format: "json" do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth"
