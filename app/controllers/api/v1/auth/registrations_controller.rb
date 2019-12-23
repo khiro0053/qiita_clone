13405ef52fd3aeea1faa97e7368549248721e7c6
khiro <khiro0053@gmail.com>
2019-12-23 22:24:33 +0900

    bundle exec rubocop -a

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   protect_from_forgery with: :null_session
 
   private
+
     def sign_up_params
       params.permit(:name, :email, :password, :password_confirmation)
     end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   namespace :api, format: "json" do
     namespace :v1 do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
-        registrations: 'api/v1/auth/registrations'
+        registrations: "api/v1/auth/registrations",
       }
       resources :articles
-
     end
   end
 end
