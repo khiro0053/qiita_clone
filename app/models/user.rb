9d248b1a72a7e659bc4682e7a44b97be88613b9a
khiro <khiro0053@gmail.com>
2020-01-04 20:33:23 +0900

    Execute password validation when password_required? is true

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   # 半角英小文字大文字数字をそれぞれ1種類以上含む半角英数字記号
   VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[!-~]+\z/.freeze
-  validates :password, presence: true, length: { in: 8..32 }, format: { with: VALID_PASSWORD_REGEX }, on: :create
+  validates :password, presence: true, length: { in: 8..32 }, format: { with: VALID_PASSWORD_REGEX }, if: :password_required?
 
   has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
