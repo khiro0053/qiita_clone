5662bb5d3d78c64eac851dec7505eee6fed11f0f
khiro <khiro0053@gmail.com>
2020-02-23 17:30:48 +0900

    Add-article-detail-page

--- app/javascript/packs/container/ArticlesContainer.vue
+++ app/javascript/packs/container/ArticlesContainer.vue
@@ -1,29 +1,31 @@
 <template>
-  <div id="articles-container">
-    <ul>
-      <li v-for="article in articles" v-bind:key="article.id">
-        <div>{{article.title}}</div>
-        <div>{{article.body}}</div>
-      </li>
-    </ul>
-  </div>
+  <v-container class="item elevation-3 articles-container"></v-container>
 </template>
 
 <script lang="ts">
 import axios from "axios";
 import { Vue, Component } from "vue-property-decorator";
-@Component
-export default class ArticlesContainer extends Vue {
-  articles: string[] = [];
-  async mounted(): Promise<void> {
-    await this.fetchArticles();
+import TimeAgo from "vue2-timeago";
+@Component({
+  components: {
+    TimeAgo
   }
-  async fetchArticles(): Promise<void> {
-    await axios.get("/api/v1/articles").then(response => {
-      response.data.map((article: any) => {
-        this.articles.push(article);
-      });
+})
+export default class ArticleContainer extends Vue {
+  article: any = "";
+  async mounted(): Promise<void> {
+    await this.fetchArticle("1");
+  }
+  async fetchArticle(id: string): Promise<void> {
+    await axios.get(`/api/v1/articles/${id}`).then(response => {
+      this.article = response.data;
     });
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.articles-container {
+  margin-top: 2em;
+}
+</style>
