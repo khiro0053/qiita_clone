f34d3da614e75f147051c5f34dd127607723c623
khiro <khiro0053@gmail.com>
2020-03-01 22:17:53 +0900

    add default status

--- db/migrate/20200301130832_add_status_to_article.rb
+++ db/migrate/20200301130832_add_status_to_article.rb
@@ -1,5 +1,5 @@
 class AddStatusToArticle < ActiveRecord::Migration[5.2]
   def change
-    add_column :articles, :status, :string
+    add_column :articles, :status, :string, default: "draft"
   end
 end
