2a9eceeb68c32fc2665f50e3dfabf93e04a41525
khiro <khiro0053@gmail.com>
2020-03-21 19:34:53 +0900

    create current/articles_controller & set routes.rb

---
+++ app/controllers/api/v1/current/articles_controller.rb
@@ -0,0 +1,7 @@
+class Api::V1::Current::ArticlesController < Api::V1::ApiController
+  before_action :authenticate_user!, only: [:index, :show]
+  def index
+    articles = current_user.articles.published
+    render json: articles
+  end
+end
